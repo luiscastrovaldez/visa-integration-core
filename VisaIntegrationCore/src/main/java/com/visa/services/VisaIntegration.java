aac0f18016c5362d3651002234436cf3fdc881db
gluiscastro <gluiscastro@78a49291-90ff-4cec-d6c6-96e4a5ab16c9>
2012-12-06 15:19:56 +0000

    added spring support

---
+++ VisaIntegrationCore/src/main/java/com/visa/services/VisaIntegration.java
@@ -0,0 +1,11 @@
+package com.visa.services;
+
+import java.io.Serializable;
+
+import com.visa.domain.LogTransaction;
+
+public interface VisaIntegration {
+
+	LogTransaction findById(Serializable id);
+	
+}
---
+++ VisaIntegrationCore/src/main/java/com/visa/services/VisaIntegrationImpl.java
@@ -0,0 +1,21 @@
+package com.visa.services;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.visa.dao.LogTransactionDAO;
+import com.visa.domain.LogTransaction;
+
+@Service("visaIntegration")
+public class VisaIntegrationImpl implements VisaIntegration {
+
+	@Autowired
+	LogTransactionDAO logTransactionDAO;
+	
+	public LogTransaction findById(Serializable id) {
+		return logTransactionDAO.findById(id);
+	}
+	
+}
