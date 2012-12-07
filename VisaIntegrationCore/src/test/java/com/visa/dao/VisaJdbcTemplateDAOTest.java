ea18362740a196a3efce5b038de89b3e75483502
gluiscastro <gluiscastro@78a49291-90ff-4cec-d6c6-96e4a5ab16c9>
2012-12-07 17:13:22 +0000

    load data from sp

--- VisaIntegrationCore/src/test/java/com/visa/dao/VisaJdbcTemplateDAOTest.java
+++ VisaIntegrationCore/src/test/java/com/visa/dao/VisaJdbcTemplateDAOTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.visa.VisaCoreTest;
 import com.visa.domain.Carrera;
+import com.visa.domain.Concepto;
 
 public class VisaJdbcTemplateDAOTest extends VisaCoreTest {
 
@@ -50,6 +51,56 @@ public class VisaJdbcTemplateDAOTest extends VisaCoreTest {
 			carreras = visaJdbcTemplateDAO.obtenerCarrerasProspecto("0001174", new Integer(1));
 			Assert.assertNotNull(carreras);
 			Assert.assertTrue(carreras.size() != 0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+		
+
+	}
+	
+	@Test
+	public void obtenerCuotasActualesTest() {
+		
+		List<Concepto> conceptos;
+		try {
+			conceptos = visaJdbcTemplateDAO.obtenerCuotasActuales("W200932473", "113");
+			Assert.assertNotNull(conceptos);
+			Assert.assertTrue(conceptos.size() != 0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+		
+
+	}
+	
+	@Test
+	public void obtenerListarCuotasPostulanteTest() {
+		
+		List<Concepto> conceptos;
+		try {
+			conceptos = visaJdbcTemplateDAO.obtenerListarCuotasPostulante("PA20032038");
+			Assert.assertNotNull(conceptos);
+			Assert.assertTrue(conceptos.size() != 0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+		
+
+	}
+	
+	
+	// single-row subquery returns more than one row
+	@Test
+	public void obtenerListarCuotasProspectoTest() {
+		
+		List<Concepto> conceptos;
+		try {
+			conceptos = visaJdbcTemplateDAO.obtenerListarCuotasProspecto("0001174", new Integer(1));
+			Assert.assertNotNull(conceptos);
+			Assert.assertTrue(conceptos.size() != 0);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
