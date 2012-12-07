6495b5507687f5189c3d75af88b2c5c39aa7f125
taniadgv@gmail.com <taniadgv@gmail.com@78a49291-90ff-4cec-d6c6-96e4a5ab16c9>
2012-12-07 14:22:11 +0000

    Adding SP domain classes

---
+++ trunk/VisaIntegrationCore/src/main/java/com/visa/domain/Concepto.java
@@ -0,0 +1,78 @@
+package com.visa.domain;
+
+public class Concepto {
+
+	private String cuota;
+	private String codigoServicio;
+	private String nombreServicio;
+	private String fechaVencimiento;
+	private String periodoacademico;
+	private String periodopago;
+	private String monto;
+	private String recibo;
+
+	public String getCuota() {
+		return cuota;
+	}
+
+	public void setCuota(String cuota) {
+		this.cuota = cuota;
+	}
+
+	public String getCodigoServicio() {
+		return codigoServicio;
+	}
+
+	public void setCodigoServicio(String codigoServicio) {
+		this.codigoServicio = codigoServicio;
+	}
+
+	public String getNombreServicio() {
+		return nombreServicio;
+	}
+
+	public void setNombreServicio(String nombreServicio) {
+		this.nombreServicio = nombreServicio;
+	}
+
+	public String getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+
+	public void setFechaVencimiento(String fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+
+	public String getPeriodoacademico() {
+		return periodoacademico;
+	}
+
+	public void setPeriodoacademico(String periodoacademico) {
+		this.periodoacademico = periodoacademico;
+	}
+
+	public String getPeriodopago() {
+		return periodopago;
+	}
+
+	public void setPeriodopago(String periodopago) {
+		this.periodopago = periodopago;
+	}
+
+	public String getMonto() {
+		return monto;
+	}
+
+	public void setMonto(String monto) {
+		this.monto = monto;
+	}
+
+	public String getRecibo() {
+		return recibo;
+	}
+
+	public void setRecibo(String recibo) {
+		this.recibo = recibo;
+	}
+
+}
