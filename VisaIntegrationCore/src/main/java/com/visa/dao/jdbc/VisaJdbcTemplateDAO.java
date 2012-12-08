16470099158c96dd845c528e4b774359e650d0f5
taniadgv <taniadgv@gmail.com>
2012-12-08 03:44:22 +0000

    Adding SP services in VisaIntegration Service

--- VisaIntegrationCore/src/main/java/com/visa/dao/jdbc/VisaJdbcTemplateDAO.java
+++ VisaIntegrationCore/src/main/java/com/visa/dao/jdbc/VisaJdbcTemplateDAO.java
@@ -1,19 +1,23 @@
 package com.visa.dao.jdbc;
 
 import java.util.List;
-import java.util.Map;
 
 import com.visa.domain.Carrera;
 import com.visa.domain.Concepto;
 
 public interface VisaJdbcTemplateDAO {
 
-	
-	List<Carrera> obtenerCarrerasPostgrado(String psAlumno) throws Exception;	
-	List<Carrera> obtenerCarrerasPostulante(String psPostulante) throws Exception;
-	List<Carrera> obtenerCarrerasProspecto(String psProspecto, Integer psAtencion) throws Exception;
-	List<Concepto> obtenerCuotasActuales(String codigoAlumno,String codigoCarrera) throws Exception;
-	List<Concepto> obtenerListarCuotasPostulante(String psPostulante) throws Exception;
-	List<Concepto> obtenerListarCuotasProspecto(String psProspecto,Integer psAtencion) throws Exception;
-	Integer verificaUsuarioExiste(String nombreUsuario, String usuarioClave) throws Exception;
+  List<Carrera> obtenerCarrerasPostgrado(String psAlumno) throws Exception;
+
+  List<Carrera> obtenerCarrerasPostulante(String psPostulante) throws Exception;
+
+  List<Carrera> obtenerCarrerasProspecto(String psProspecto, Integer psAtencion) throws Exception;
+
+  List<Concepto> obtenerCuotasActuales(String codigoAlumno, String codigoCarrera) throws Exception;
+
+  List<Concepto> obtenerListarCuotasPostulante(String psPostulante) throws Exception;
+
+  List<Concepto> obtenerListarCuotasProspecto(String psProspecto, Integer psAtencion) throws Exception;
+
+  Integer verificaUsuarioExiste(String nombreUsuario, String usuarioClave) throws Exception;
 }
--- VisaIntegrationCore/src/test/java/com/visa/dao/VisaJdbcTemplateDAOTest.java
+++ VisaIntegrationCore/src/test/java/com/visa/dao/VisaJdbcTemplateDAOTest.java
@@ -7,125 +7,118 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.visa.VisaCoreTest;
+import com.visa.dao.jdbc.VisaJdbcTemplateDAO;
 import com.visa.domain.Carrera;
 import com.visa.domain.Concepto;
 
 public class VisaJdbcTemplateDAOTest extends VisaCoreTest {
 
-	@Autowired
-	com.visa.dao.jdbc.VisaJdbcTemplateDAO visaJdbcTemplateDAO;
+  @Autowired
+  VisaJdbcTemplateDAO visaJdbcTemplateDAO;
 
-	@Test
-	public void obtenerCarrerasPostgradoTest() {
-		List<Carrera> carreras;
-		try {
-			carreras = visaJdbcTemplateDAO.obtenerCarrerasPostgrado("CL20031506");
-			Assert.assertNotNull(carreras);
-			Assert.assertTrue(carreras.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+  @Test
+  public void obtenerCarrerasPostgradoTest() {
+    List<Carrera> carreras;
+    try {
+      carreras = visaJdbcTemplateDAO.obtenerCarrerasPostgrado("CL20031506");
+      Assert.assertNotNull(carreras);
+      Assert.assertTrue(carreras.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
 
-	}
-	@Test
-	public void obtenerCarrerasPostulanteTest() {
-		List<Carrera> carreras;
-		try {
-			carreras = visaJdbcTemplateDAO.obtenerCarrerasPostulante("PA20032038");
-			Assert.assertNotNull(carreras);
-			Assert.assertTrue(carreras.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+  }
 
-	}
-	@Test
-	public void obtenerCarrerasProspectoTest() {
-		
-		List<Carrera> carreras;
-		try {
-			carreras = visaJdbcTemplateDAO.obtenerCarrerasProspecto("0001174", new Integer(1));
-			Assert.assertNotNull(carreras);
-			Assert.assertTrue(carreras.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		
+  @Test
+  public void obtenerCarrerasPostulanteTest() {
+    List<Carrera> carreras;
+    try {
+      carreras = visaJdbcTemplateDAO.obtenerCarrerasPostulante("PA20032038");
+      Assert.assertNotNull(carreras);
+      Assert.assertTrue(carreras.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
 
-	}
-	
-	@Test
-	public void obtenerCuotasActualesTest() {
-		
-		List<Concepto> conceptos;
-		try {
-			conceptos = visaJdbcTemplateDAO.obtenerCuotasActuales("W200932473", "113");
-			Assert.assertNotNull(conceptos);
-			Assert.assertTrue(conceptos.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		
+  }
 
-	}
-	
-	@Test
-	public void obtenerListarCuotasPostulanteTest() {
-		
-		List<Concepto> conceptos;
-		try {
-			conceptos = visaJdbcTemplateDAO.obtenerListarCuotasPostulante("PA20032038");
-			Assert.assertNotNull(conceptos);
-			Assert.assertTrue(conceptos.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		
+  @Test
+  public void obtenerCarrerasProspectoTest() {
 
-	}
-	
-	
-	// single-row subquery returns more than one row
-	@Test
-	public void obtenerListarCuotasProspectoTest() {
-		
-		List<Concepto> conceptos;
-		try {
-			conceptos = visaJdbcTemplateDAO.obtenerListarCuotasProspecto("0001174", new Integer(1));
-			Assert.assertNotNull(conceptos);
-			Assert.assertTrue(conceptos.size() != 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		
-		
-	}
-	
-	@Test
-	public void verificaUsuarioExisteTest() {
-		
-		Integer flag;
-		try {
-			flag = visaJdbcTemplateDAO.verificaUsuarioExiste("a2012900222","bif87933");
-			System.out.println(" flag " + flag);
-			Assert.assertNotNull(flag);
-			Assert.assertTrue(flag > 0);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}				
-		
-		
-	}
-	
-	
-	
+    List<Carrera> carreras;
+    try {
+      carreras = visaJdbcTemplateDAO.obtenerCarrerasProspecto("0001174", new Integer(1));
+      Assert.assertNotNull(carreras);
+      Assert.assertTrue(carreras.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+
+  }
+
+  @Test
+  public void obtenerCuotasActualesTest() {
+
+    List<Concepto> conceptos;
+    try {
+      conceptos = visaJdbcTemplateDAO.obtenerCuotasActuales("W200932473", "113");
+      Assert.assertNotNull(conceptos);
+      Assert.assertTrue(conceptos.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+
+  }
+
+  @Test
+  public void obtenerListarCuotasPostulanteTest() {
+
+    List<Concepto> conceptos;
+    try {
+      conceptos = visaJdbcTemplateDAO.obtenerListarCuotasPostulante("PA20032038");
+      Assert.assertNotNull(conceptos);
+      Assert.assertTrue(conceptos.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+
+  }
+
+  // single-row subquery returns more than one row
+  @Test
+  public void obtenerListarCuotasProspectoTest() {
+
+    List<Concepto> conceptos;
+    try {
+      conceptos = visaJdbcTemplateDAO.obtenerListarCuotasProspecto("0001174", new Integer(1));
+      Assert.assertNotNull(conceptos);
+      Assert.assertTrue(conceptos.size() != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+
+  }
+
+  @Test
+  public void verificaUsuarioExisteTest() {
+
+    Integer flag;
+    try {
+      flag = visaJdbcTemplateDAO.verificaUsuarioExiste("a2012900222", "bif87933");
+      System.out.println(" flag " + flag);
+      Assert.assertNotNull(flag);
+      Assert.assertTrue(flag > 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+
+  }
+
 }
