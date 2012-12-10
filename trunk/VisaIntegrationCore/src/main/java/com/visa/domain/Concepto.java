be7ebf3bb0ec1d5494095cc36cdc6d3f6692491c
gluiscastro <gluiscastro@78a49291-90ff-4cec-d6c6-96e4a5ab16c9>
2012-12-10 03:43:46 +0000

    order contructor conceptos

--- trunk/VisaIntegrationCore/src/main/java/com/visa/domain/Concepto.java
+++ trunk/VisaIntegrationCore/src/main/java/com/visa/domain/Concepto.java
@@ -7,101 +7,119 @@ import org.springframework.jdbc.core.RowMapper;
 
 public class Concepto implements RowMapper<Object> {
 
-  private String cuota;
-  private String codigoServicio;
-  private String nombreServicio;
-  private String fechaVencimiento;
-  private String periodoacademico;
-  private String periodopago;
-  private String monto;
-  private String recibo;
+	private int item;
+	private String cuota;
+	private String codigoServicio;
+	private String nombreServicio;
+	private String fechaVencimiento;
+	private String periodoacademico;
+	private String periodopago;
+	private String monto;
+	private String recibo;
 
-  public Concepto() {
+	public Concepto() {
 
-  }
+	}
 
-  public Concepto(String cuota, String codigoServicio, String nombreServicio, String fechaVencimiento, String periodoacademico, String periodopago,
-      String monto, String recibo) {
-    super();
-    this.cuota = cuota;
-    this.codigoServicio = codigoServicio;
-    this.nombreServicio = nombreServicio;
-    this.fechaVencimiento = fechaVencimiento;
-    this.periodoacademico = periodoacademico;
-    this.periodopago = periodopago;
-    this.monto = monto;
-    this.recibo = recibo;
-  }
+	public Concepto(int item, String cuota, String codigoServicio,
+			String nombreServicio, String fechaVencimiento,
+			String periodoacademico, String periodopago, String monto,
+			String recibo) {
+		super();
+		this.item = item;
+		this.cuota = cuota;
+		this.codigoServicio = codigoServicio;
+		this.nombreServicio = nombreServicio;
+		this.fechaVencimiento = fechaVencimiento;
+		this.periodoacademico = periodoacademico;
+		this.periodopago = periodopago;
+		this.monto = monto;
+		this.recibo = recibo;
+	}
 
-  public String getCuota() {
-    return cuota;
-  }
+	public String getCuota() {
+		return cuota;
+	}
 
-  public void setCuota(String cuota) {
-    this.cuota = cuota;
-  }
+	public void setCuota(String cuota) {
+		this.cuota = cuota;
+	}
 
-  public String getCodigoServicio() {
-    return codigoServicio;
-  }
+	public String getCodigoServicio() {
+		return codigoServicio;
+	}
 
-  public void setCodigoServicio(String codigoServicio) {
-    this.codigoServicio = codigoServicio;
-  }
+	public void setCodigoServicio(String codigoServicio) {
+		this.codigoServicio = codigoServicio;
+	}
 
-  public String getNombreServicio() {
-    return nombreServicio;
-  }
+	public String getNombreServicio() {
+		return nombreServicio;
+	}
 
-  public void setNombreServicio(String nombreServicio) {
-    this.nombreServicio = nombreServicio;
-  }
+	public void setNombreServicio(String nombreServicio) {
+		this.nombreServicio = nombreServicio;
+	}
 
-  public String getFechaVencimiento() {
-    return fechaVencimiento;
-  }
+	public String getFechaVencimiento() {
+		return fechaVencimiento;
+	}
 
-  public void setFechaVencimiento(String fechaVencimiento) {
-    this.fechaVencimiento = fechaVencimiento;
-  }
+	public void setFechaVencimiento(String fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
 
-  public String getPeriodoacademico() {
-    return periodoacademico;
-  }
+	public String getPeriodoacademico() {
+		return periodoacademico;
+	}
 
-  public void setPeriodoacademico(String periodoacademico) {
-    this.periodoacademico = periodoacademico;
-  }
+	public void setPeriodoacademico(String periodoacademico) {
+		this.periodoacademico = periodoacademico;
+	}
 
-  public String getPeriodopago() {
-    return periodopago;
-  }
+	public String getPeriodopago() {
+		return periodopago;
+	}
 
-  public void setPeriodopago(String periodopago) {
-    this.periodopago = periodopago;
-  }
+	public void setPeriodopago(String periodopago) {
+		this.periodopago = periodopago;
+	}
 
-  public String getMonto() {
-    return monto;
-  }
+	public String getMonto() {
+		return monto;
+	}
 
-  public void setMonto(String monto) {
-    this.monto = monto;
-  }
+	public void setMonto(String monto) {
+		this.monto = monto;
+	}
 
-  public String getRecibo() {
-    return recibo;
-  }
+	public String getRecibo() {
+		return recibo;
+	}
 
-  public void setRecibo(String recibo) {
-    this.recibo = recibo;
-  }
+	public void setRecibo(String recibo) {
+		this.recibo = recibo;
+	}
 
-  @Override
-  public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
-    Concepto concepto = new Concepto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6),
-        rs.getString(7), rs.getString(8));
-    return concepto;
-  }
+	public int getItem() {
+		return item;
+	}
+
+	public void setItem(int item) {
+		this.item = item;
+	}
+
+	
+	
+	
+	@Override
+	public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Concepto concepto = new Concepto(rs.getInt(1), rs.getString(2),
+				rs.getString(3), rs.getString(4), rs.getString(5),
+				rs.getString(6), rs.getString(7), rs.getString(8),
+				rs.getString(9));
+				
+		return concepto;
+	}
 
 }
