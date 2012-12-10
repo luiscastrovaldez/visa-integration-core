1a9a1613ee127529c8a17a55c451d77e09f7b0de
taniadgv <taniadgv@gmail.com>
2012-12-10 01:57:02 +0000

    Fixing Concepts SP

--- VisaIntegrationCore/src/main/java/com/visa/domain/Concepto.java
+++ VisaIntegrationCore/src/main/java/com/visa/domain/Concepto.java
@@ -7,96 +7,101 @@ import org.springframework.jdbc.core.RowMapper;
 
 public class Concepto implements RowMapper<Object> {
 
-	private String cuota;
-	private String codigoServicio;
-	private String nombreServicio;
-	private String fechaVencimiento;
-	private String periodoacademico;
-	private String periodopago;
-	private String monto;
-	private String recibo;
+  private String cuota;
+  private String codigoServicio;
+  private String nombreServicio;
+  private String fechaVencimiento;
+  private String periodoacademico;
+  private String periodopago;
+  private String monto;
+  private String recibo;
 
-	public Concepto(String cuota, String codigoServicio, String nombreServicio, String fechaVencimiento, String periodoacademico, String periodopago,
-			String monto, String recibo) {
-		super();
-		this.cuota = cuota;
-		this.codigoServicio = codigoServicio;
-		this.nombreServicio = nombreServicio;
-		this.fechaVencimiento = fechaVencimiento;
-		this.periodoacademico = periodoacademico;
-		this.periodopago = periodopago;
-		this.monto = monto;
-		this.recibo = recibo;
-	}
+  public Concepto() {
 
-	public String getCuota() {
-		return cuota;
-	}
+  }
 
-	public void setCuota(String cuota) {
-		this.cuota = cuota;
-	}
+  public Concepto(String cuota, String codigoServicio, String nombreServicio, String fechaVencimiento, String periodoacademico, String periodopago,
+      String monto, String recibo) {
+    super();
+    this.cuota = cuota;
+    this.codigoServicio = codigoServicio;
+    this.nombreServicio = nombreServicio;
+    this.fechaVencimiento = fechaVencimiento;
+    this.periodoacademico = periodoacademico;
+    this.periodopago = periodopago;
+    this.monto = monto;
+    this.recibo = recibo;
+  }
 
-	public String getCodigoServicio() {
-		return codigoServicio;
-	}
+  public String getCuota() {
+    return cuota;
+  }
 
-	public void setCodigoServicio(String codigoServicio) {
-		this.codigoServicio = codigoServicio;
-	}
+  public void setCuota(String cuota) {
+    this.cuota = cuota;
+  }
 
-	public String getNombreServicio() {
-		return nombreServicio;
-	}
+  public String getCodigoServicio() {
+    return codigoServicio;
+  }
 
-	public void setNombreServicio(String nombreServicio) {
-		this.nombreServicio = nombreServicio;
-	}
+  public void setCodigoServicio(String codigoServicio) {
+    this.codigoServicio = codigoServicio;
+  }
 
-	public String getFechaVencimiento() {
-		return fechaVencimiento;
-	}
+  public String getNombreServicio() {
+    return nombreServicio;
+  }
 
-	public void setFechaVencimiento(String fechaVencimiento) {
-		this.fechaVencimiento = fechaVencimiento;
-	}
+  public void setNombreServicio(String nombreServicio) {
+    this.nombreServicio = nombreServicio;
+  }
 
-	public String getPeriodoacademico() {
-		return periodoacademico;
-	}
+  public String getFechaVencimiento() {
+    return fechaVencimiento;
+  }
 
-	public void setPeriodoacademico(String periodoacademico) {
-		this.periodoacademico = periodoacademico;
-	}
+  public void setFechaVencimiento(String fechaVencimiento) {
+    this.fechaVencimiento = fechaVencimiento;
+  }
 
-	public String getPeriodopago() {
-		return periodopago;
-	}
+  public String getPeriodoacademico() {
+    return periodoacademico;
+  }
 
-	public void setPeriodopago(String periodopago) {
-		this.periodopago = periodopago;
-	}
+  public void setPeriodoacademico(String periodoacademico) {
+    this.periodoacademico = periodoacademico;
+  }
 
-	public String getMonto() {
-		return monto;
-	}
+  public String getPeriodopago() {
+    return periodopago;
+  }
 
-	public void setMonto(String monto) {
-		this.monto = monto;
-	}
+  public void setPeriodopago(String periodopago) {
+    this.periodopago = periodopago;
+  }
 
-	public String getRecibo() {
-		return recibo;
-	}
+  public String getMonto() {
+    return monto;
+  }
 
-	public void setRecibo(String recibo) {
-		this.recibo = recibo;
-	}
+  public void setMonto(String monto) {
+    this.monto = monto;
+  }
 
-	@Override
-	public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  public String getRecibo() {
+    return recibo;
+  }
+
+  public void setRecibo(String recibo) {
+    this.recibo = recibo;
+  }
+
+  @Override
+  public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+    Concepto concepto = new Concepto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6),
+        rs.getString(7), rs.getString(8));
+    return concepto;
+  }
 
 }
