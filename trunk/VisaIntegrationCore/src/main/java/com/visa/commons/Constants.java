53d76e4d7f58267f060c7d1edf024e6946108098
taniadgv@gmail.com <taniadgv@gmail.com@78a49291-90ff-4cec-d6c6-96e4a5ab16c9>
2012-12-13 23:13:41 +0000

    Added required constants and bean for visa operations

--- trunk/VisaIntegrationCore/src/main/java/com/visa/commons/Constants.java
+++ trunk/VisaIntegrationCore/src/main/java/com/visa/commons/Constants.java
@@ -11,7 +11,7 @@ public final class Constants {
 	public static String SF_VERIFICAUSUARIOEXISTE = "Pkg_Sso.SF_VERIFICAUSUARIOEXISTE";
 	public static String SF_VERIFICARPOSTULANTEEXISTE = "Pkg_Sso.SF_VERIFICARPOSTULANTEEXISTE";
 	public static String SF_VERIFICARPROSPECTOEXISTE = "Pkg_Sso.SF_VERIFICARPROSPECTOEXISTE";
-	
+
 	public static String SPI_REGISTRATRANVISA = "PKG_PAGOVISA.SPI_REGISTRATRANVISA";
 	public static String SPI_REGISTRATRANVISADET = "PKG_PAGOVISA.SPI_REGISTRATRANVISADET";
 	public static String SPS_OBTENERMONTOTRANVISA = "PKG_PAGOVISA.SPS_OBTENERMONTOTRANVISA";
@@ -24,8 +24,8 @@ public final class Constants {
 	public static String SPS_CUOTASPREGRADO = "PKG_PAGOVISA.SPS_CUOTASPREGRADO";
 	public static String SPS_CUOTAS = "PKG_PAGOVISA.SPS_CUOTAS";
 	public static String SF_OBTENERCARRERAPREGRADO = "PKG_PAGOVISA.SF_OBTENERCARRERAPREGRADO";
-	
-	
+	public static String SPI_REGISTRATRANVISARESPUESTA = "PKG_PAGOVISA.SPI_REGISTRATRANVISARESPUESTA";
+
 	public static String PS_CLIENTE = "PS_CLIENTE";
 	public static String PS_PERIODOAC = "PS_PERIODOAC";
 	public static String PN_ATENCION = "PN_ATENCION";
@@ -37,30 +37,37 @@ public final class Constants {
 	public static String PS_PERIODOPAGO = "PS_PERIODOPAGO";
 	public static String PS_ALUMNO = "PS_ALUMNO";
 	public static String PS_POSTULANTE = "PS_POSTULANTE";
-	public static String PS_CARRERA = "PS_CARRERA";	
-	public static String PS_PROSPECTO = "PS_PROSPECTO";	
+	public static String PS_CARRERA = "PS_CARRERA";
+	public static String PS_PROSPECTO = "PS_PROSPECTO";
 	public static String PS_USUARIO = "PS_USUARIO";
 	public static String PS_CLAVE = "PS_CLAVE";
 	public static String PS_DOMINIO = "PS_DOMINIO";
 	public static String PS_ESTADO = "PS_ESTADO";
 	public static String PD_FECHATRAN = "PD_FECHATRAN";
-	
-	
+	public static String PS_RESPUESTA = "PS_RESPUESTA";
+	public static String PS_CODTIENDA = "PS_CODTIENDA";
+	public static String PS_NORDENT = "PS_NORDENT";
+	public static String PS_CODACCION = "PS_CODACCION";
+	public static String PS_PAN = "PS_PAN";
+	public static String PS_ECI = "PS_ECI";
+	public static String PS_CODAUTORIZA = "PS_CODAUTORIZA";
+	public static String PS_ORITARJETA = "PS_ORITARJETA";
+	public static String PS_NOMEMISOR = "PS_NOMEMISOR";
+	public static String PS_DESCECI = "PS_DESCECI";
 
 	public static String FLAG_USUARIO = "FLAG_USUARIO";
 	public static String RETURN_VALUE = "RETURN_VALUE";
 	public static String PS_CODIGO = "PS_CODIGO";
 	public static String PS_ID = "PS_ID";
 	public static String PS_EMAIL = "PS_EMAIL";
-	
+
 	public static String P_CURSOR = "pCursor";
-	
+
 	public static String DOMINIO = "uwiener.edu.pe";
-	
-	public static int INTINSTITUCION = 51;    // Universidad Norbert Wiener
-	public static int PERFIL_ALUMNO = 2; 
-	
+
+	public static int INTINSTITUCION = 51; // Universidad Norbert Wiener
+	public static int PERFIL_ALUMNO = 2;
+
 	public static int INT_POSTULANTE = 0;
-	
 
 }
---
+++ trunk/VisaIntegrationCore/src/main/java/com/visa/domain/TranVisaRespuesta.java
@@ -0,0 +1,204 @@
+package com.visa.domain;
+
+public class TranVisaRespuesta {
+
+	private String estado;
+	private String alumno;
+	private String carrera;
+	private String respuesta;
+	private String codTienda;
+	private String nOrdenT;
+	private String codAccion;
+	private String pan;
+	private String nombreTh;
+	private String eci;
+	private String codAutoriza;
+	private String oriTarjeta;
+	private String nomEmisor;
+	private String dscEci;
+	private String codRescvv2;
+	private String impAutorizado;
+	private String fechaHoraTx;
+	private String fechaHoraDeposito;
+	private String fechaHoraDevolucion;
+	private String datoComercio;
+	private String descripcionProducto;
+	private String descripcionCodigo;
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(String alumno) {
+		this.alumno = alumno;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+
+	public String getRespuesta() {
+		return respuesta;
+	}
+
+	public void setRespuesta(String respuesta) {
+		this.respuesta = respuesta;
+	}
+
+	public String getCodTienda() {
+		return codTienda;
+	}
+
+	public void setCodTienda(String codTienda) {
+		this.codTienda = codTienda;
+	}
+
+	public String getnOrdenT() {
+		return nOrdenT;
+	}
+
+	public void setnOrdenT(String nOrdenT) {
+		this.nOrdenT = nOrdenT;
+	}
+
+	public String getCodAccion() {
+		return codAccion;
+	}
+
+	public void setCodAccion(String codAccion) {
+		this.codAccion = codAccion;
+	}
+
+	public String getPan() {
+		return pan;
+	}
+
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+
+	public String getNombreTh() {
+		return nombreTh;
+	}
+
+	public void setNombreTh(String nombreTh) {
+		this.nombreTh = nombreTh;
+	}
+
+	public String getEci() {
+		return eci;
+	}
+
+	public void setEci(String eci) {
+		this.eci = eci;
+	}
+
+	public String getCodAutoriza() {
+		return codAutoriza;
+	}
+
+	public void setCodAutoriza(String codAutoriza) {
+		this.codAutoriza = codAutoriza;
+	}
+
+	public String getOriTarjeta() {
+		return oriTarjeta;
+	}
+
+	public void setOriTarjeta(String oriTarjeta) {
+		this.oriTarjeta = oriTarjeta;
+	}
+
+	public String getNomEmisor() {
+		return nomEmisor;
+	}
+
+	public void setNomEmisor(String nomEmisor) {
+		this.nomEmisor = nomEmisor;
+	}
+
+	public String getDscEci() {
+		return dscEci;
+	}
+
+	public void setDscEci(String dscEci) {
+		this.dscEci = dscEci;
+	}
+
+	public String getCodRescvv2() {
+		return codRescvv2;
+	}
+
+	public void setCodRescvv2(String codRescvv2) {
+		this.codRescvv2 = codRescvv2;
+	}
+
+	public String getImpAutorizado() {
+		return impAutorizado;
+	}
+
+	public void setImpAutorizado(String impAutorizado) {
+		this.impAutorizado = impAutorizado;
+	}
+
+	public String getFechaHoraTx() {
+		return fechaHoraTx;
+	}
+
+	public void setFechaHoraTx(String fechaHoraTx) {
+		this.fechaHoraTx = fechaHoraTx;
+	}
+
+	public String getFechaHoraDeposito() {
+		return fechaHoraDeposito;
+	}
+
+	public void setFechaHoraDeposito(String fechaHoraDeposito) {
+		this.fechaHoraDeposito = fechaHoraDeposito;
+	}
+
+	public String getFechaHoraDevolucion() {
+		return fechaHoraDevolucion;
+	}
+
+	public void setFechaHoraDevolucion(String fechaHoraDevolucion) {
+		this.fechaHoraDevolucion = fechaHoraDevolucion;
+	}
+
+	public String getDatoComercio() {
+		return datoComercio;
+	}
+
+	public void setDatoComercio(String datoComercio) {
+		this.datoComercio = datoComercio;
+	}
+
+	public String getDescripcionProducto() {
+		return descripcionProducto;
+	}
+
+	public void setDescripcionProducto(String descripcionProducto) {
+		this.descripcionProducto = descripcionProducto;
+	}
+
+	public String getDescripcionCodigo() {
+		return descripcionCodigo;
+	}
+
+	public void setDescripcionCodigo(String descripcionCodigo) {
+		this.descripcionCodigo = descripcionCodigo;
+	}
+
+}
